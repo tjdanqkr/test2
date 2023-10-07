dafe78a383c408ba60a56cdf4dfd4447772c3a5b
tjdanqkr <tjdanqkr@gmail.com>
2023-10-07 19:13:38 +0900

    first commit

---
+++ src/main/java/com/example/jenkins/JenkinsApplication.java
@@ -0,0 +1,16 @@
+package com.example.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class JenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsApplication.class, args);
+    }
+
+}
+
---
+++ src/main/java/com/example/jenkins/JenkinsController.java
@@ -0,0 +1,13 @@
+package com.example.jenkins;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JenkinsController {
+
+    @GetMapping
+    public String jenkins() {
+        return "Hello";
+    }
+}
