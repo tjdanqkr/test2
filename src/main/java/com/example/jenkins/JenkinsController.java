118daa389432c157c5387bbedbe02635e1d7bd93
tjdanqkr <49061542+tjdanqkr@users.noreply.github.com>
2023-10-09 17:06:22 +0900

    Update JenkinsController.java

--- src/main/java/com/example/jenkins/JenkinsController.java
+++ src/main/java/com/example/jenkins/JenkinsController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class JenkinsController {
 
-    @GetMapping
+    @GetMapping //
     public String jenkins() {
         return "Hello3";
     }
